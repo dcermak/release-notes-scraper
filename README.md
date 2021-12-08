6c47af46593077a528e89fe8b6ece56a6ab85116
Dan Čermák <dcermak@suse.com>
2021-12-08 14:29:30 +0100

    HACK: create a super simple release notes scraper from github

---
+++ README.md
@@ -0,0 +1,39 @@
+# release-notes-scraper
+
+This simple script can be used to grab the release notes for projects from
+github that do not keep a `CHANGELOG`, but publish their release notes via the
+releases page.
+
+
+## Prerequisites
+
+You will need the following tools:
+
+- Python 3.6.2 or later
+- [`poetry`](https://python-poetry.org/)
+
+Additionally, you have to clone this repository and initialize poetry:
+```ShellSession
+❯ git clone https://github.com/dcermak/release-notes-scraper.git
+❯ poetry install
+```
+
+## Usage
+
+To grab the changelog of a the project `octocat/foobar`, run:
+
+```ShellSession
+❯ poetry run ./release-notes-scraper.py octocat foobar
+1.0.5
+
+- We did cool stuff!
+```
+
+You can also specify the maximum version to be included in the output via the
+CLI flag `--max-version` and the minimum version that should **not** be included
+(i.e. the next release after `$min_version` will be present) in the output via
+the CLI flag `--min-version`:
+
+```ShellSession
+❯ poetry run ./release-notes-scraper.py octocat foobar --min-version 1.0.5
+```
---
+++ release-notes-scraper.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+from dataclasses import dataclass
+from typing import List, Optional, Union
+import sys
+
+import requests
+from packaging.version import InvalidVersion, LegacyVersion, Version, parse
+
+
+@dataclass
+class Release:
+    version: str
+    changelog: str
+
+    def __post_init__(self):
+        self.changelog = self.changelog.replace("\r\n", "\n")
+
+    def __str__(self) -> str:
+        return f"""{self.version}:
+
+{self.changelog}
+"""
+
+
+def fetch_releases(
+    owner: str,
+    repository: str,
+    min_version: Optional[Union[Version, LegacyVersion]] = None,
+    max_version: Optional[Union[Version, LegacyVersion]] = None,
+) -> List[Release]:
+    resp = requests.get(
+        f"https://api.github.com/repos/{owner}/{repository}/releases?per_page=100"
+    )
+
+    releases: List[Release] = []
+
+    for release in resp.json():
+        ver = None
+        ver_str = release["name"].replace("v", "")
+        try:
+            ver = parse(ver_str)
+        except InvalidVersion as iv_err:
+            print(
+                f"skipping version {ver_str}, not a valid semver, got the following error while parsing: {str(iv_err)}",
+                file=sys.stderr,
+            )
+            continue
+        assert isinstance(ver, (Version, LegacyVersion))
+
+        if min_version is not None and ver <= min_version:
+            continue
+        if max_version is not None and ver > max_version:
+            continue
+
+        releases.append(Release(version=ver_str, changelog=release["body"]))
+
+    return releases
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Grab the release changelog from Github for a given repository and an optional version constraint"
+    )
+
+    parser.add_argument("owner", type=str, nargs=1, help="The github repository owner")
+    parser.add_argument(
+        "repository", type=str, nargs=1, help="The github repository name"
+    )
+
+    parser.add_argument(
+        "--min-version",
+        type=str,
+        nargs=1,
+        help="The minimum version that should *not* be included in the changelog",
+        default=[None],
+    )
+    parser.add_argument(
+        "--max-version",
+        type=str,
+        nargs=1,
+        help="The maximum version that should be included in the changelog",
+        default=[None],
+    )
+
+    args = parser.parse_args()
+
+    min_version = None
+    if args.min_version[0] is not None:
+        min_version = parse(args.min_version[0])
+    max_version = None
+    if args.max_version[0] is not None:
+        max_version = parse(args.max_version[0])
+
+    if (
+        min_version is not None
+        and max_version is not None
+        and min_version > max_version
+    ):
+        raise ValueError(
+            f"minimal version {min_version} is larger than maximum version {max_version}"
+        )
+
+    for rel in fetch_releases(
+        args.owner[0],
+        args.repository[0],
+        min_version,
+        max_version,
+    ):
+        print(rel)
